772122f04b7867b9359231b878c8960b96c4111e
Anukul Sangwan <anukulsangwan@icloud.com>
2021-04-09 16:00:46 +0530

    copy: show snackbar message

--- src/components/Copy/index.js
+++ src/components/Copy/index.js
@@ -1,12 +1,25 @@
 import './index.css';
 import * as PropTypes from 'prop-types';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { showSnackbar } from '../../actions/snackbar';
+import { useDispatch } from 'react-redux';
 import Icon from '../Icon';
 import React from 'react';
 
 const Copy = ({ text }) => {
+    const dispatch = useDispatch();
+
+    const showSnack = () => {
+        dispatch(showSnackbar({
+            message: `Copied to clipboard: ${text}`,
+        }));
+    };
+
     return (
-        <CopyToClipboard text={text}>
+        <CopyToClipboard
+            text={text}
+            onCopy={showSnack}
+        >
             <div className="copy-section">
                 <div className="flex-center">
                     <Icon
